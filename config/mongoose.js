f43a210959136a57ee6724b92f30a7ee87039bce
Shalloo Bajpai <39648828+Shalu6bajpai@users.noreply.github.com>
2020-07-24 16:06:30 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/hospital", {
+mongoose.connect("mongodb://localhost/hospital-Api", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
@@ -8,10 +8,10 @@ mongoose.connect("mongodb://localhost/hospital", {
 
 const db = mongoose.connection;
 
-db.on("error", console.error.bind(console, "Error connecting to mongo DB"));
+db.on("error", console.error.bind(console, "Error connecting to DB"));
 
 db.once("open", () => {
-  console.log("Database connected!:: MONGODB");
+  console.log("Database connected!!!!!!:: Mongo-Db");
 });
 
 module.exports = db;
